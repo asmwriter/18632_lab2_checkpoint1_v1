3d31101fbb88913979f3a23890c1afaf94ee4a2e
Anusha Raghavendra <araghave@andrew.cmu.edu>
2023-04-07 15:49:03 -0400

    added testevector read code

--- aes_128_lab2_v2/aes_tb.v
+++ aes_128_lab2_v2/aes_tb.v
@@ -4,6 +4,8 @@
 `define CMD_ST 2'b01
 `define CMD_SK 2'b10
 `define CMD_SP 2'b11
+`define ITER_NUM 500 //get from text file
+
 
 module aes_tb(
     
@@ -13,30 +15,42 @@ module aes_tb(
 reg clk = 1'b0;
 always #5 clk = !clk;
 
+
+reg [127:0] plain_text;
+reg [127:0] input_key;
+reg [127:0] cipher_text;
+
 //reg [127:0] plain_text = 128'h 00041214120412000c00131108231919;
-//reg [127:0] key = 128'h 2475a2b33475568831e2120013aa5487;
-//reg [7:0] din;
-//reg [1:0] cmd;
+//reg [127:0] input_key = 128'h 2475a2b33475568831e2120013aa5487;
+//reg [127:0] cipher_text = 128'h_69c4e0d86a7b0430d8cdb78070b4c55a;
 reg rst_ = 1'b1;
 wire ok;
 wire ready;
 wire [7:0] dout;
 reg e128;
 
+reg[31:0] vectornum, errors;   // bookkeeping variables 
+reg[3:0]  testvectors[10000:0];// array of testvectors
+reg[3:0]  testvectors1[10000:0];// array of testvectors
+reg[3:0]  testvectors2[10000:0];// array of testvectors
+
+
 //aescipher u1(.clk(clk),.din(din),.cmd(cmd),.ok(ok),.ready(ready),.dout(dout),.rst_(rst_));
-aescipher u1(.clk(clk),.ok(ok),.ready(ready),.dout(dout),.rst_(rst_), .e128(e128));
+aescipher u1(.clk(clk),.ok(ok),.ready(ready),.dout(dout),.rst_(rst_), .e128(e128), .input_key(input_key), .plain_text(plain_text), .cipher_text(cipher_text));
 integer i;
+/*
 initial begin
     #20
     rst_ = 1'b0;
     #20
     rst_ = 1'b1;
     #1000;
-	
+
 $display("OUTPUT: e128:%d\n", e128);
 
 $finish;	
-    
+end
+*/    
    /* 
     cmd = `CMD_SP;
     #10
@@ -62,6 +76,44 @@ $finish;
    #20
    cmd = `CMD_ID;
     */
+
+initial begin            
+// Will execute at the beginning once begin 
+$readmemh("input_key.txt", testvectors); // Read vectors 
+$readmemh("plain_text.txt", testvectors1); // Read vectors 
+$readmemh("cipher_text.txt", testvectors2); // Read vectors 
+vectornum= 0; errors = 0;// Initialize 
+rst_ = 1; #27; rst_ = 0;// Apply rst_ wait 
+end
+
+// apply test vectors on rising edge of 
+always @(posedge clk) begin 
+#1;
+if (~rst_) begin
+if (ok) begin 
+ {input_key, plain_text, cipher_text} = {testvectors[vectornum], testvectors1[vectornum], testvectors2[vectornum]}; 
+end
+end
+end
+
+
+// check results on falling edge of clk 
+always @(posedge clk)  begin 
+//always @(posedge ok)  begin 
+if (ok) begin
+if (~rst_)   begin            // skip during rst_ begin 
+if (e128 !== 1) begin  
+$display("Error: inputs = %b", {input_key, plain_text, cipher_text}); 
+$display("  outputs vectornum=%b, e128= %b", vectornum, e128); 
+errors = errors + 1; 
+end	//if 
+vectornum= vectornum+ 1; 
+if (testvectors[vectornum] === `ITER_NUM) begin 
+$display("%d tests completed with %d errors", vectornum, errors); 
+$finish;// End simulation end end
+end
+end
+end
 end
 
 endmodule
--- aes_128_lab2_v2/csrc/filelist.cu
+++ aes_128_lab2_v2/csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _22213_archive_1.so
-_22213_archive_1.so : archive.8/_22213_archive_1.a
+ARCHIVE_OBJS += _29626_archive_1.so
+_29626_archive_1.so : archive.13/_29626_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_22213_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_29626_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_22213_archive_1.so $@
+	@ln -sf .//../simv.daidir//_29626_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.8/_prev_archive_1.a
+_prev_archive_1.so : archive.13/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
