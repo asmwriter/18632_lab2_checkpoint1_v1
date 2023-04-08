573b2de3709d3ba411bd0120a8e74449fa5eba0f
Ganesh Venkatachalam <ganeshece1317@gmail.com>
2023-04-08 13:11:21 -0400

    ok = 1 now, design was always reset and complt_ was not set to 0 after 10 rounds

--- aes_128_lab2_v2/aes_tb.v
+++ aes_128_lab2_v2/aes_tb.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ns
+`timescale 1ns / 1ps
 
 `define CMD_ID 2'b00
 `define CMD_ST 2'b01
@@ -11,11 +11,7 @@ module aes_tb(
     
 );
 
-// 100MHz clk
-reg clk = 1'b0;
-always #5 clk = !clk;
-
-
+logic clk;
 reg [127:0] plain_text;
 reg [127:0] input_key;
 reg [127:0] cipher_text;
@@ -29,10 +25,11 @@ wire ready;
 wire [7:0] dout;
 reg e128;
 
+reg first_round;
 reg[31:0] vectornum, errors;   // bookkeeping variables 
-reg[3:0]  testvectors[10000:0];// array of testvectors
-reg[3:0]  testvectors1[10000:0];// array of testvectors
-reg[3:0]  testvectors2[10000:0];// array of testvectors
+reg[127:0]  testvectors[127:0];// array of testvectors
+reg[127:0]  testvectors1[127:0];// array of testvectors
+reg[217:0]  testvectors2[127:0];// array of testvectors
 
 
 //aescipher u1(.clk(clk),.din(din),.cmd(cmd),.ok(ok),.ready(ready),.dout(dout),.rst_(rst_));
@@ -77,43 +74,71 @@ end
    cmd = `CMD_ID;
     */
 
+initial begin
+	clk = 1'b1;
+	forever #10ns clk = ~clk;
+end
+
 initial begin            
 // Will execute at the beginning once begin 
-$readmemh("input_key.txt", testvectors); // Read vectors 
-$readmemh("plain_text.txt", testvectors1); // Read vectors 
-$readmemh("cipher_text.txt", testvectors2); // Read vectors 
-vectornum= 0; errors = 0;// Initialize 
-rst_ = 1; #27; rst_ = 0;// Apply rst_ wait 
+	
+	$monitor("time=%d,clk:%x", $time, clk);
+	$display("input");
+	$readmemh("input_key.txt", testvectors); // Read vectors 
+	$display("plain");
+	$readmemh("plain_text.txt", testvectors1); // Read vectors 
+	$display("cipher");
+	$readmemh("cipher_text.txt", testvectors2); // Read vectors 
+	vectornum= 0; errors = 0;// Initialize 
+
+	first_round = 1'b1;
+	rst_ = 0; #100; rst_ = 1;// Apply rst_ wait 
+
+#10000
+$finish;
 end
+
+
 
 // apply test vectors on rising edge of 
 always @(posedge clk) begin 
-#1;
-if (~rst_) begin
-if (ok) begin 
- {input_key, plain_text, cipher_text} = {testvectors[vectornum], testvectors1[vectornum], testvectors2[vectornum]}; 
-end
-end
+	#1;
+	if (~rst_) begin
+		if (ok || first_round) begin 
+	 		{input_key, plain_text, cipher_text} = {testvectors[vectornum], testvectors1[vectornum], testvectors2[vectornum]}; 
+			first_round <= 1'b0;
+			vectornum <= vectornum + 1; 
+		end
+	end
+	$display("state=%x", u1.state_cnt);
+	$display("state_reg=%x", u1.state_reg);
+	$display("key_reg=%x", u1.key_reg);
+	$display("vectornum=%x, e128=%x, ok=%x\n",vectornum,e128,ok);
+	$display("input: plaintext[vectornum]=%x",testvectors1[vectornum]);
+	$display("input: key[vectornum]=%x",testvectors[vectornum]);
+	$display("output ciphertext[vectornum]=%x",cipher_text);
 end
 
 
 // check results on falling edge of clk 
-always @(posedge clk)  begin 
+always @(negedge clk)  begin 
 //always @(posedge ok)  begin 
-if (ok) begin
-if (~rst_)   begin            // skip during rst_ begin 
-if (e128 !== 1) begin  
-$display("Error: inputs = %b", {input_key, plain_text, cipher_text}); 
-$display("  outputs vectornum=%b, e128= %b", vectornum, e128); 
-errors = errors + 1; 
-end	//if 
-vectornum= vectornum+ 1; 
-if (testvectors[vectornum] === `ITER_NUM) begin 
-$display("%d tests completed with %d errors", vectornum, errors); 
-$finish;// End simulation end end
-end
-end
-end
+	if (ok) begin
+		if (~rst_)   begin            // skip during rst_ begin 
+			if (e128 !== 1) begin  
+				$display("Error: inputs = %b", {input_key, plain_text, cipher_text}); 
+				$display("  outputs vectornum=%b, e128= %b", vectornum, e128); 
+				errors = errors + 1; 
+			end	//if
+		    else begin
+				$display("vectornum=%x, e128=%x\n",vectornum,e128);
+			end	
+			
+			if (testvectors[vectornum] === `ITER_NUM) begin 
+				$display("%d tests completed with %d errors", vectornum, errors); 
+			end
+		end
+	end
 end
 
 endmodule
